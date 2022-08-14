9617e68672f7ea57d8be9b838b7c64c69fa708ca
Weronika Kędziora <weronika.kedziora@edgeteq.com>
2022-08-14 20:55:48 +0200

    feat: init front

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Biuro rachunkowe Adamo" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped></style>

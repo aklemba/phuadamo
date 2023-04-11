8fc944a8f1b03aec4d37f881d0dc49a1b742f197
Weronika Kędziora <weronika.kedziora@edgeteq.com>
2023-04-11 22:33:52 +0200

    price list section

---
+++ src/components/priceList/PriceList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex">
+    <header class="flex-1 text-center">
+      <h2 class="section-title text-5xl text-left">Cennik</h2>
+    </header>
+
+    <div class="flex-1">
+      <p>
+        Ceny oferowanych usług ustalamy indywidualnie dla każdego Klienta na podstawie przewidywanej liczby dokumentów, liczby zatrudnionych pracowników, formy opodatkowania, na podstawie indywidulanych oczekiwań i potrzeb Klienta oraz przewidywanego czasu wykonania zleconych usług.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.offer-section {
+  padding: 6rem 0;
+}
+</style>

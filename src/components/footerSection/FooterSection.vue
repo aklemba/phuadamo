c2adea0ad5d7e6bab1b56cd904270dc09f868884
Weronika Kędziora <weronika.kedziora@edgeteq.com>
2023-05-15 22:08:09 +0200

    sections mobile style

--- src/components/footerSection/FooterSection.vue
+++ src/components/footerSection/FooterSection.vue
@@ -30,7 +30,11 @@
 <style scoped lang="scss">
 .footer {
   @apply bg-stone-200 border-t px-2 sm:px-4 py-2.5 sticky w-full z-20 top-0 left-0 border-b border-gray-200 text-gray-700;
-  padding: 5rem;
+  padding: 2rem;
+
+  @media (min-width: 72rem) {
+    padding: 5rem;
+  }
   // background: #e8e4e4;
   // background: rgba(200, 188, 170, 0.7);
   // background: hsla(5, 15%, 80%, 0.7);
